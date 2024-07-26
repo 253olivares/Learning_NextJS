0d2935b65b1a9732ee85894a2194cb9c5e4fee58
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-07-25 19:16:26 -0600

    New modules

---
+++ Practice2_InstallingNextJs/app/components/like-button.js
@@ -0,0 +1,14 @@
+// create component has a client component
+'use client';
+
+import { useState } from "react";
+
+export default function LikeButton() {
+
+    const [likes,setLikes] = useState(0);
+
+    function handleClick() {
+        setLikes(likes=> likes+1);
+    }
+    return <button onClick={handleClick}>Like({likes})</button>
+}
